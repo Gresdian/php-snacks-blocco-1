ec44bf6d8c3d6640ea37393d3fbae22d3e3b1466
Gresdian <chris_da98@outlook.it>
2024-01-24 21:13:29 +0100

    [update] snack3 finished

---
+++ snack_3/snack3.php
@@ -0,0 +1,67 @@
+<?php
+
+$posts = [
+
+    '15/07/2023' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '03/12/2023' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '07/01/2024' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php foreach($posts as $date => $post) { ?>
+         <h1><?php echo $date ?></h1>
+         <?php foreach($post as $p) { ?>
+            <ul>
+               <?php foreach ($p as $key => $value) { ?>
+                  <li>
+                     <b><?php echo $key ?></b>
+                     <span><?php echo $value ?></span>
+                  </li>
+               <?php } ?>
+            </ul>
+         <?php } ?>
+    <?php } ?>
+</body>
+</html>
